2ad4e93db160a1b8607bf3db2d5db05dea6a1ad0
Domas Poliakas <domas@poliakas.dev>
2021-12-07 10:44:38 +0100

    seven

--- 07/src/main/scala/dpol/Main.scala
+++ 07/src/main/scala/dpol/Main.scala
@@ -7,10 +7,55 @@ import fs2._
 import fs2.io.file._
 import cats.parse._
 
-object Main extends AocApp(3, DoPart.One):
+object Main extends AocAppStreamed(7, DoPart.Two):
 
-  def part1(inputFile: Path): IO[String] =
-    IO.unit
+  val csvparse =
+    Rfc5234.digit.rep.map(_.mkString_("").toInt).repSep(Parser.char(','))
 
-  def part2(inputFile: Path): IO[String] =
-    IO.unit
+  def part1(inputFile: Stream[IO, String]): IO[String] =
+    inputFile
+      .filter(_.nonEmpty)
+      .flatMap(v =>
+        Stream.fromEither[IO](
+          csvparse.parse(v).leftMap(_ => new Exception("hek"))
+        )
+      )
+      .map(_._2)
+      .map(l =>
+        val max = l.maximum
+        val min = l.minimum
+
+        val aa = (for (a <- min to max) yield {
+          a -> (l.map(v => Math.abs(v - a)).sumAll)
+        }).minBy(_._2)
+
+        aa
+      )
+      .compile
+      .last
+
+  def part2(inputFile: Stream[IO, String]): IO[String] =
+
+    def cost(i: Int): Int = 
+      ((Math.pow(i, 2) + i) / 2).toInt
+
+    inputFile
+      .filter(_.nonEmpty)
+      .flatMap(v =>
+        Stream.fromEither[IO](
+          csvparse.parse(v).leftMap(_ => new Exception("hek"))
+        )
+      )
+      .map(_._2)
+      .map(l =>
+        val max = l.maximum
+        val min = l.minimum
+
+        val aa = (for (a <- min to max) yield {
+          a -> (l.map(v => cost(Math.abs(v - a))).sumAll)
+        }).minBy(_._2)
+
+        aa
+      )
+      .compile
+      .last
