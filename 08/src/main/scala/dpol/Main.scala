5b8ab6a8f804b099ae27d10177400384965e8b73
Domas Poliakas <domas@poliakas.dev>
2021-12-09 11:11:59 +0100

    08 and 09

--- 08/src/main/scala/dpol/Main.scala
+++ 08/src/main/scala/dpol/Main.scala
@@ -6,11 +6,159 @@ import cats.syntax.all._
 import fs2._
 import fs2.io.file._
 import cats.parse._
+import cats.data.NonEmptyList
+import cats.data.NonEmptySet
+import cats.kernel.Order
 
-object Main extends AocApp(3, DoPart.One):
+object Main extends AocAppStreamed(8, DoPart.Two):
 
-  def part1(inputFile: Path): IO[String] =
-    IO.unit
+  val parser =
+    (
+      (Rfc5234.alpha.rep <* Rfc5234.sp).repUntil(Parser.char('|')) <* Parser
+        .char('|') <* Rfc5234.sp,
+      (Rfc5234.alpha.rep <* Rfc5234.sp.rep0).rep(4, 4)
+    ).tupled
 
-  def part2(inputFile: Path): IO[String] =
-    IO.unit
+  def part1(inputFile: Stream[IO, String]): IO[String] =
+    inputFile
+      .filter(_.nonEmpty)
+      .evalMap(s =>
+        IO.fromEither(parser.parse(s).leftMap(e => new Exception(s"hek: $e")))
+      )
+      .map(_._2._2)
+      .map(
+        _.toList.filter { v =>
+          val size = v.size
+          size == 2 || size == 4 || size == 3 || size == 7
+        }
+      )
+      .map(_.size)
+      .compile
+      .foldMonoid
+
+  val numberWires =
+    Map(
+      0 -> List('a', 'b', 'c', 'e', 'f', 'g'),
+      1 -> List('c', 'f'),
+      2 -> List('a', 'c', 'd', 'e', 'g'),
+      3 -> List('a', 'c', 'd', 'f', 'g'),
+      4 -> List('b', 'c', 'd', 'f'),
+      5 -> List('a', 'b', 'd', 'f', 'g'),
+      6 -> List('a', 'b', 'd', 'e', 'f', 'g'),
+      7 -> List('a', 'c', 'f'),
+      8 -> List('a', 'b', 'c', 'd', 'e', 'f', 'g'),
+      9 -> List('a', 'b', 'c', 'd', 'f', 'g')
+    )
+
+  val wireNumbers = numberWires.toList
+    .map(_.swap)
+    .toMap
+
+  def dedooce(
+      inputs: NonEmptyList[NonEmptyList[Char]],
+      availableNumbers: List[Int],
+      currentMappings: Map[Char, Char],
+      unmapped: List[Char]
+  ): List[Map[String, Int]] =
+
+    println(inputs)
+    println(availableNumbers)
+    println(currentMappings)
+    println(unmapped)
+
+    def configurations(
+        wires: List[Char],
+        boop: List[Char]
+    ): List[List[(Char, Char)]] =
+      def go(
+          back: List[Char],
+          forward: List[Char],
+          l: Char
+      ): List[((Char, Char), List[Char])] = forward match
+        case Nil => Nil
+        case h :: t =>
+          ((l -> h) -> (back ::: t)) :: go(back ::: List(h), t, l)
+
+      wires match
+        case Nil => Nil
+        case h :: t =>
+          val a = go(Nil, boop, h)
+          a.flatMap((one, others) =>
+            configurations(t, others) match
+              case Nil => List(List(one))
+              case t   => t.map(one :: _)
+          )
+
+    inputs match {
+      case NonEmptyList(h, t) =>
+        val l = h.size match
+          case 2 => List(1)
+          case 3 => List(7)
+          case 4 => List(4)
+          case 5 => List(2, 3, 5)
+          case 6 => List(0, 6, 9)
+          case 7 => List(8)
+
+        val ll = l
+          .filter(v => availableNumbers.contains(v))
+          .flatMap { (num) =>
+
+            val v = numberWires(num)
+
+            val (unfound, found) = v.partitionEither(v =>
+              currentMappings.get(v).tupleLeft(v).toRight(v)
+            )
+
+            val newMappings =
+              if (unfound.isEmpty)
+                List(currentMappings)
+              else
+                configurations(unfound, unmapped).map(
+                  _.toMap ++ currentMappings
+                )
+
+            NonEmptyList.fromList(t) match
+              case None =>
+                newMappings.map(newMapping =>
+                  wireNumbers.toList
+                    .map((wires, number) =>
+                      wires.map(newMapping.apply).mkString -> number
+                    )
+                    .toMap
+                )
+
+              case Some(x) =>
+                newMappings.flatMap(newMapping =>
+                  dedooce(
+                    x,
+                    availableNumbers.filter(_ != num),
+                    newMapping,
+                    unmapped.filterNot(newMapping.contains(_))
+                  )
+                )
+
+          }
+
+        ll
+
+    }
+
+  def part2(inputFile: Stream[IO, String]): IO[String] =
+    inputFile
+      .filter(_.nonEmpty)
+      .evalMap(s =>
+        IO.fromEither(parser.parse(s).leftMap(e => new Exception(s"hek: $e")))
+      )
+      .map { v =>
+        val m = dedooce(
+          v._2._1,
+          (0 to 9).toList,
+          Map.empty,
+          List('a', 'b', 'c', 'd', 'e', 'f', 'g')
+        )
+        val f = m.head
+        v._2._2.map(v => f(v.mkString_("")))
+      }
+      .debug()
+      .compile
+      .drain
--- 09/src/main/scala/dpol/Main.scala
+++ 09/src/main/scala/dpol/Main.scala
@@ -6,11 +6,156 @@ import cats.syntax.all._
 import fs2._
 import fs2.io.file._
 import cats.parse._
+import cats.data.NonEmptyList
+import scala.annotation.tailrec
 
-object Main extends AocApp(3, DoPart.One):
+enum Basin:
+  case HighPoint
+  case LowPoint
+  case Dir(x: Int, y: Int)
 
-  def part1(inputFile: Path): IO[String] =
-    IO.unit
+object Main extends AocAppStreamed(9, DoPart.Two):
 
-  def part2(inputFile: Path): IO[String] =
-    IO.unit
+  def part1(inputFile: Stream[IO, String]): IO[String] =
+    inputFile
+      .filter(_.nonEmpty)
+      .evalMap(a =>
+        IO.fromEither(
+          Rfc5234.digit.rep
+            .map(_.map(_.toString.toInt))
+            .parse(a)
+            .leftMap(e => new Exception(e.toString))
+        ).map(_._2)
+      )
+      .zipWithPreviousAndNext
+      .map { case (previous, current, next) =>
+        ((none[Int], none[Int], none[Int]) ::
+
+          (previous
+            .fold(List.empty[Int])(_.toList)
+            .padZipWith(
+              current.toList
+                .padZip(next.fold(List.empty[Int])(_.toList))
+            ) { (p, b) =>
+              val (c, n) = b.unzip
+              (p, c.flatten, n.flatten)
+            }) ::: List((none[Int], none[Int], none[Int]))).sliding(3).map {
+          case a :: (tm, mm, bm) :: b :: Nil =>
+            val others = a.toList ::: List(tm, bm) ::: b.toList
+
+            mm.filter(i => others.forall(_.fold(true)(_ > i)))
+
+          case _ => None
+        }
+      }
+      .flatMap(v => Stream.emits(v.toSeq))
+      .unNone
+      .map(_ + 1)
+      .compile
+      .foldMonoid
+
+  def part2(inputFile: Stream[IO, String]): IO[String] =
+    inputFile
+      .filter(_.nonEmpty)
+      .evalMap(a =>
+        IO.fromEither(
+          Rfc5234.digit.rep
+            .map(_.map(_.toString.toInt))
+            .parse(a)
+            .leftMap(e => new Exception(e.toString))
+        ).map(_._2)
+      )
+      .zipWithIndex
+      .zipWithPreviousAndNext
+      .map { case (previous, (currentRow, currentIndex), next) =>
+
+        val (previousRow, previousIndex) = previous
+          .map(_.bimap(_.toList, _.some))
+          .getOrElse(List.empty[Int] -> None)
+        val (nextRow, nextIndex) =
+          next.map(_.bimap(_.toList, _.some)).getOrElse(List.empty[Int] -> None)
+
+        ((
+          none[(Int, (Int, Long))],
+          none[(Int, (Int, Long))],
+          none[(Int, (Int, Long))]
+        ) ::
+          (previousRow.zipWithIndex
+            .map((value, index) => value -> (index, previousIndex.get))
+            .padZipWith(
+              currentRow.toList.zipWithIndex
+                .map((value, index) => value -> (index, currentIndex))
+                .padZip(
+                  nextRow.zipWithIndex.map((value, index) =>
+                    value -> (index, nextIndex.get)
+                  )
+                )
+            ) { (p, b) =>
+              val (c, n) = b.unzip
+              (p, c.flatten, n.flatten)
+            }) ::: List(
+            (
+              none[(Int, (Int, Long))],
+              none[(Int, (Int, Long))],
+              none[(Int, (Int, Long))]
+            )
+          )).sliding(3).map {
+          case a :: (tm, mm, bm) :: b :: Nil =>
+
+            mm.map {
+              case (9, coord) => 
+                coord -> Basin.HighPoint
+              case (value, coord) =>
+                NonEmptyList.fromList(List(a._2, tm, bm, b._2).collect {
+                  case Some(neightbourValue, neighbourCoord) if neightbourValue < value => neightbourValue -> neighbourCoord
+                  }).fold(coord -> Basin.LowPoint){v => 
+                  val (_, dirCoord) = v.minimumBy(_._1)
+                  coord -> Basin.Dir(dirCoord._1, dirCoord._2.toInt)
+                }
+            }
+
+          case _ => None
+        }
+
+      }
+      .flatMap(v => Stream.emits(v.toSeq))
+      .unNone
+      .compile
+      .toList
+      .map { v =>
+        val mapp = v.toMap
+
+        @tailrec
+        def follow(x: Int, y: Int): Option[(Int, Int)] =
+          mapp.get((x, y.toLong)) match 
+            case None => None
+            case Some(Basin.HighPoint) => None
+            case Some(Basin.LowPoint) => Some(x -> y)
+            case Some(Basin.Dir(xx, yy)) => follow(xx, yy)
+
+        val res = v.foldLeft(Map.empty[(Int, Int), Int])((acc, next) =>
+            follow(next._1._1, next._1._2.toInt).fold(acc)((a) => acc.updatedWith(a)(_.fold(1)(_ + 1).some))
+            )
+
+        val res2 = res.toList.sortBy(_._2).reverse
+
+        (res2(0)._2 * res2(1)._2 * res2(2)._2).toString
+
+      }
+
+// inputFile
+//   .filter(_.nonEmpty)
+//   .evalMap(a =>
+//     IO.fromEither(
+//       Rfc5234.digit.rep
+//         .map(_.map(_.toString.toInt))
+//         .parse(a)
+//         .leftMap(e => new Exception(e.toString))
+//     ).map(_._2)
+//   )
+//   .compile
+//   .toList.map(
+//     grid =>
+//       Map.empty[(Int, Int), Either[(Int, Int), Int]]
+
+//       )
