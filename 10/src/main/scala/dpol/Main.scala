48835b90113a8e9575f24b3bb6ea65dc63fca53c
Domas Poliakas <domas@poliakas.dev>
2021-12-11 13:11:29 +0100

    10 and 11

--- 10/src/main/scala/dpol/Main.scala
+++ 10/src/main/scala/dpol/Main.scala
@@ -6,11 +6,109 @@ import cats.syntax.all._
 import fs2._
 import fs2.io.file._
 import cats.parse._
+import cats.data.NonEmptyList
+import scala.annotation.tailrec
 
-object Main extends AocApp(3, DoPart.One):
+object Main extends AocAppStreamed(10, DoPart.Two):
 
-  def part1(inputFile: Path): IO[String] =
-    IO.unit
+  enum Brack:
+    case Parens(l: List[Brack])
+    case Square(l: List[Brack])
+    case Curly(l: List[Brack])
+    case Angle(l: List[Brack])
 
-  def part2(inputFile: Path): IO[String] =
-    IO.unit
+  def fullo = brack.rep <* Parser.end
+
+  def brack: Parser[Brack] =
+    (brackP('<', '>', Brack.Angle(_)) orElse
+      brackP('(', ')', Brack.Parens(_)) orElse
+      brackP('[', ']', Brack.Square(_)) orElse
+      brackP('{', '}', Brack.Curly(_)))
+
+  def brackP(open: Char, close: Char, f: List[Brack] => Brack) =
+    (Parser
+      .char(open)
+      .flatMap(_ => brack.rep0.withContext(close.toString))
+      .flatMap(a => Parser.char(close).withContext(close.toString).as(a)))
+      .map(f)
+
+  def part1(inputFile: Stream[IO, String]): IO[String] =
+    inputFile
+      .filter(_.nonEmpty)
+      .map { v =>
+        val r = brack.parse(v)
+        println(r)
+        r match
+          case Left(Parser.Error(offset, _)) if offset < v.length =>
+            v.charAt(offset) match
+              case ')' => 3
+              case ']' => 57
+              case '}' => 1197
+              case '>' => 25137
+              case _   => 0
+          case _ => 0
+      }
+      .compile
+      .foldMonoid
+
+  def part2(inputFile: Stream[IO, String]): IO[String] =
+
+    def charScore(c: Char): Int = c match {
+      case ')' => 1
+      case ']' => 2
+      case '}' => 3
+      case '>' => 4
+    }
+
+    def calco(s: List[Char], acc: Long): Long = s match
+      case Nil    => acc
+      case h :: t => calco(t, acc * 5 + charScore(h))
+
+    @tailrec
+    def parsoo(
+        s: List[Char],
+        stack: List[Char]
+    ): Either[(String, List[Char]), List[Char]] =
+      s match
+        case h :: t =>
+          h match
+            case l @ ('(' | '<' | '[' | '{') => parsoo(t, l :: stack)
+            case ')' =>
+              stack match
+                case '(' :: tt => parsoo(t, tt)
+                case o         => Left(s.mkString -> o)
+            case '>' =>
+              stack match
+                case '<' :: tt => parsoo(t, tt)
+                case o         => Left(s.mkString -> o)
+            case ']' =>
+              stack match
+                case '[' :: tt => parsoo(t, tt)
+                case o         => Left(s.mkString -> o)
+            case '}' =>
+              stack match
+                case '{' :: tt => parsoo(t, tt)
+                case o         => Left(s.mkString -> o)
+        case Nil => Right(stack)
+
+    def convert(i: Char): Char = i match
+      case '(' => ')'
+      case '[' => ']'
+      case '{' => '}'
+      case '<' => '>'
+      case i   => i
+
+    inputFile
+      .filter(_.nonEmpty)
+      .map { v =>
+        parsoo(v.toCharArray.toList, Nil) match
+          case Right(l) if l.nonEmpty =>
+            v -> Some(l.map(convert))
+          case _ => v -> None
+      }
+      .map(_._2)
+      .unNone
+      .map(l => calco(l, 0))
+      .compile
+      .toList
+      .map(l => l.sorted.drop(l.size / 2).head.toString)
--- 11/src/main/scala/dpol/Main.scala
+++ 11/src/main/scala/dpol/Main.scala
@@ -6,11 +6,79 @@ import cats.syntax.all._
 import fs2._
 import fs2.io.file._
 import cats.parse._
+import cats.data.State
 
-object Main extends AocApp(3, DoPart.One):
+object Main extends AocAppStreamed(11, DoPart.Two):
 
-  def part1(inputFile: Path): IO[String] =
-    IO.unit
+  val mods =
+    List(-1 -> -1, 0 -> -1, 1 -> -1, -1 -> 0, 1 -> 0, -1 -> 1, 0 -> 1, 1 -> 1)
 
-  def part2(inputFile: Path): IO[String] =
-    IO.unit
+  def step(grid: Map[(Int, Int), Int]): State[Int, Map[(Int, Int), Int]] =
+    def go(g: Map[(Int, Int), Int]): State[Int, Map[(Int, Int), Int]] =
+      val (flashing, unflashed) = g.partition(_._2 > 9)
+      State
+        .modify[Int](_ + flashing.size)
+        .flatMap(_ =>
+          if (flashing.nonEmpty)
+            val n = flashing.toList
+              .flatMap((point, _) =>
+                mods.map((x, y) => (point._1 + x) -> (point._2 + y))
+              )
+              .foldLeft(unflashed)((acc, n) => acc.updatedWith(n)(_.map(_ + 1)))
+            go(n).map(_ ++ flashing.view.mapValues(_ => 0))
+          else State.pure(unflashed)
+        )
+
+    val g = grid.view.mapValues(_ + 1)
+    go(g.toMap)
+
+  def printGrid(g: Map[(Int, Int), Int]): String = g
+    .groupBy(_._1._2)
+    .toList
+    .sortBy(_._1)
+    .map((_, b) => b.toList.sortBy(_._1._1).map(_._2))
+    .map(_.mkString)
+    .mkString("*****\n", "\n", "\n")
+
+  def part1(inputFile: Stream[IO, String]): IO[String] =
+    inputFile
+      .filter(_.nonEmpty)
+      .map(_.toCharArray.toList.map(_.toString.toInt))
+      .compile
+      .toList
+      .map(gridL =>
+        val grid = gridL
+          .map(_.zipWithIndex)
+          .zipWithIndex
+          .flatMap((l, y) => l.map((v, x) => (x, y) -> v))
+          .toMap
+
+          val n = (1 to 100).foldLeft(grid -> 0)((acc, _) => step(acc._1).run(acc._2).value.swap)
+
+          n._2.toString
+
+      )
+
+  def part2(inputFile: Stream[IO, String]): IO[String] =
+    inputFile
+      .filter(_.nonEmpty)
+      .map(_.toCharArray.toList.map(_.toString.toInt))
+      .compile
+      .toList
+      .map(gridL =>
+        val grid = gridL
+          .map(_.zipWithIndex)
+          .zipWithIndex
+          .flatMap((l, y) => l.map((v, x) => (x, y) -> v))
+          .toMap
+
+        def go(g: Map[(Int, Int), Int], n: Int): Int =
+          val m = step(g).run(0).value._2
+          if (m.forall(_._2 == 0))
+            n
+          else
+            go(m, n + 1)
+
+        go(grid, 1).toString
+
+      )
